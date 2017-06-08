ElisDN/yii2-demo-shop

Added catalog actions

shop/readModels/Shop/ProductReadRepository.php
@@ -1,0 +1,80 @@
+<?php
+
+namespace shop\readModels\Shop;
+
+use shop\entities\Shop\Brand;
+use shop\entities\Shop\Category;
+use shop\entities\Shop\Product\Product;
+use shop\entities\Shop\Tag;
+use yii\data\ActiveDataProvider;
+use yii\data\DataProviderInterface;
+use yii\db\ActiveQuery;
+use yii\helpers\ArrayHelper;
+
+class ProductReadRepository
+{
+    public function getAll(): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
+        return $this->getProvider($query);
+    }
+
+    public function getAllByCategory(Category $category): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto', 'category');
+        $ids = ArrayHelper::merge([$category->id], $category->getLeaves()->select('id')->column());
+        $query->joinWith(['categoryAssignments ca'], false);
+        $query->andWhere(['or', ['p.category_id' => $ids], ['ca.category_id' => $ids]]);
+        $query->groupBy('p.id');
+        return $this->getProvider($query);
+    }
+
+    public function getAllByBrand(Brand $brand): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
+        $query->andWhere(['p.brand_id' => $brand->id]);
+        return $this->getProvider($query);
+    }
+
+    public function getAllByTag(Tag $tag): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
+        $query->joinWith(['tagAssignments ta'], false);
+        $query->andWhere(['ta.tag_id' => $tag->id]);
+        $query->groupBy('p.id');
+        return $this->getProvider($query);
+    }
+
+    public function find($id): ?Product
+    {
+        return Product::find()->active()->andWhere(['id' => $id])->one();
+    }
+
+    private function getProvider(ActiveQuery $query): ActiveDataProvider
+    {
+        return new ActiveDataProvider([
+            'query' => $query,
+            'sort' => [
+                'defaultOrder' => ['id' => SORT_DESC],
+                'attributes' => [
+                    'id' => [
+                        'asc' => ['p.id' => SORT_ASC],
+                        'desc' => ['p.id' => SORT_DESC],
+                    ],
+                    'name' => [
+                        'asc' => ['p.name' => SORT_ASC],
+                        'desc' => ['p.name' => SORT_DESC],
+                    ],
+                    'price' => [
+                        'asc' => ['p.price_new' => SORT_ASC],
+                        'desc' => ['p.price_new' => SORT_DESC],
+                    ],
+                    'rating' => [
+                        'asc' => ['p.rating' => SORT_ASC],
+                        'desc' => ['p.rating' => SORT_DESC],
+                    ],
+                ],
+            ],
+        ]);
+    }
+}

akulyk/charte-bar

shop/readModels/Shop/ProductReadRepository.php
@@ -1,0 +1,80 @@
+<?php
+
+namespace shop\readModels\Shop;
+
+use shop\entities\Shop\Brand;
+use shop\entities\Shop\Category;
+use shop\entities\Shop\Product\Product;
+use shop\entities\Shop\Tag;
+use yii\data\ActiveDataProvider;
+use yii\data\DataProviderInterface;
+use yii\db\ActiveQuery;
+use yii\helpers\ArrayHelper;
+
+class ProductReadRepository
+{
+    public function getAll(): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
+        return $this->getProvider($query);
+    }
+
+    public function getAllByCategory(Category $category): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto', 'category');
+        $ids = ArrayHelper::merge([$category->id], $category->getDescendants()->select('id')->column());
+        $query->joinWith(['categoryAssignments ca'], false);
+        $query->andWhere(['or', ['p.category_id' => $ids], ['ca.category_id' => $ids]]);
+        $query->groupBy('p.id');
+        return $this->getProvider($query);
+    }
+
+    public function getAllByBrand(Brand $brand): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
+        $query->andWhere(['p.brand_id' => $brand->id]);
+        return $this->getProvider($query);
+    }
+
+    public function getAllByTag(Tag $tag): DataProviderInterface
+    {
+        $query = Product::find()->alias('p')->active('p')->with('mainPhoto');
+        $query->joinWith(['tagAssignments ta'], false);
+        $query->andWhere(['ta.tag_id' => $tag->id]);
+        $query->groupBy('p.id');
+        return $this->getProvider($query);
+    }
+
+    public function find($id): ?Product
+    {
+        return Product::find()->active()->andWhere(['id' => $id])->one();
+    }
+
+    private function getProvider(ActiveQuery $query): ActiveDataProvider
+    {
+        return new ActiveDataProvider([
+            'query' => $query,
+            'sort' => [
+                'defaultOrder' => ['id' => SORT_DESC],
+                'attributes' => [
+                    'id' => [
+                        'asc' => ['p.id' => SORT_ASC],
+                        'desc' => ['p.id' => SORT_DESC],
+                    ],
+                    'name' => [
+                        'asc' => ['p.name' => SORT_ASC],
+                        'desc' => ['p.name' => SORT_DESC],
+                    ],
+                    'price' => [
+                        'asc' => ['p.price_new' => SORT_ASC],
+                        'desc' => ['p.price_new' => SORT_DESC],
+                    ],
+                    'rating' => [
+                        'asc' => ['p.rating' => SORT_ASC],
+                        'desc' => ['p.rating' => SORT_DESC],
+                    ],
+                ],
+            ],
+        ]);
+    }
+}