ElisDN/yii2-demo-shop

Added brand view

frontend/views/shop/catalog/brand.php
@@ -1,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\DataProviderInterface */
+/* @var $brand shop\entities\Shop\Brand */
+
+use yii\helpers\Html;
+
+$this->title = $brand->getSeoTile();
+
+$this->registerMetaTag(['name' =>'description', 'content' => $brand->meta->description]);
+$this->registerMetaTag(['name' =>'keywords', 'content' => $brand->meta->keywords]);
+
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $brand->name;
+?>
+
+<h1><?= Html::encode($brand->name) ?></h1>
+
+<hr />
+
+<?= $this->render('_list', [
+    'dataProvider' => $dataProvider
+]) ?>
+
+

shop/entities/Shop/Brand.php
@@ -32,6 +32,11 @@ class Brand extends ActiveRecord
         $this->meta = $meta;
     }
 
+    public function getSeoTile(): string
+    {
+        return $this->meta->title ?: $this->name;
+    }
+
     ##########################
 
     public static function tableName(): string

akulyk/charte-bar

frontend/views/shop/catalog/brand.php
@@ -1,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\DataProviderInterface */
+/* @var $brand shop\entities\Shop\Brand */
+
+use yii\helpers\Html;
+
+$this->title = $brand->getSeoTitle();
+
+$this->registerMetaTag(['name' =>'description', 'content' => $brand->meta->description]);
+$this->registerMetaTag(['name' =>'keywords', 'content' => $brand->meta->keywords]);
+
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $brand->name;
+?>
+
+<h1><?= Html::encode($brand->name) ?></h1>
+
+<hr />
+
+<?= $this->render('_list', [
+    'dataProvider' => $dataProvider
+]) ?>
+
+

shop/entities/Shop/Brand.php
@@ -32,6 +32,11 @@ class Brand extends ActiveRecord
         $this->meta = $meta;
     }
 
+    public function getSeoTitle(): string
+    {
+        return $this->meta->title ?: $this->name;
+    }
+
     ##########################
 
     public static function tableName(): string