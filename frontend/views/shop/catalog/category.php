ElisDN/yii2-demo-shop

Added category view

frontend/views/shop/catalog/category.php
@@ -1,0 +1,36 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\DataProviderInterface */
+/* @var $category shop\entities\Shop\Category */
+
+use yii\helpers\Html;
+
+$this->title = $category->getSeoTile();
+
+$this->registerMetaTag(['name' =>'description', 'content' => $category->meta->description]);
+$this->registerMetaTag(['name' =>'keywords', 'content' => $category->meta->keywords]);
+
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $category->name;
+?>
+
+<h1><?= Html::encode($category->getHeadingTile()) ?></h1>
+
+<?= $this->render('_subcategories', [
+    'category' => $category
+]) ?>
+
+<?php if (trim($category->description)): ?>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <?= Yii::$app->formatter->asNtext($category->description) ?>
+        </div>
+    </div>
+<?php endif; ?>
+
+<?= $this->render('_list', [
+    'dataProvider' => $dataProvider
+]) ?>
+
+

shop/entities/Shop/Category.php
@@ -49,6 +49,16 @@ class Category extends ActiveRecord
         $this->meta = $meta;
     }
 
+    public function getSeoTile(): string
+    {
+        return $this->meta->title ?: $this->getHeadingTile();
+    }
+
+    public function getHeadingTile(): string
+    {
+        return $this->title ?: $this->name;
+    }
+
     public static function tableName(): string
     {
         return '{{%shop_categories}}';

akulyk/charte-bar

frontend/views/shop/catalog/category.php
@@ -1,0 +1,36 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\DataProviderInterface */
+/* @var $category shop\entities\Shop\Category */
+
+use yii\helpers\Html;
+
+$this->title = $category->getSeoTitle();
+
+$this->registerMetaTag(['name' =>'description', 'content' => $category->meta->description]);
+$this->registerMetaTag(['name' =>'keywords', 'content' => $category->meta->keywords]);
+
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $category->name;
+?>
+
+<h1><?= Html::encode($category->getHeadingTile()) ?></h1>
+
+<?= $this->render('_subcategories', [
+    'category' => $category
+]) ?>
+
+<?php if (trim($category->description)): ?>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <?= Yii::$app->formatter->asNtext($category->description) ?>
+        </div>
+    </div>
+<?php endif; ?>
+
+<?= $this->render('_list', [
+    'dataProvider' => $dataProvider
+]) ?>
+
+

shop/entities/Shop/Category.php
@@ -49,6 +49,16 @@ class Category extends ActiveRecord
         $this->meta = $meta;
     }
 
+    public function getSeoTitle(): string
+    {
+        return $this->meta->title ?: $this->getHeadingTile();
+    }
+
+    public function getHeadingTile(): string
+    {
+        return $this->title ?: $this->name;
+    }
+
     public static function tableName(): string
     {
         return '{{%shop_categories}}';