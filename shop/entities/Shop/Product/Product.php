ElisDN/yii2-demo-shop

Added product information
Added product information *

shop/entities/Shop/Product/Product.php
@@ -110,6 +110,11 @@ class Product extends ActiveRecord
         return $this->status == self::STATUS_DRAFT;
     }
 
+    public function getSeoTile(): string
+    {
+        return $this->meta->title ?: $this->name;
+    }
+
     public function setValue($id, $value): void
     {
         $values = $this->values;

akulyk/charte-bar

shop/entities/Shop/Product/Product.php
@@ -110,6 +110,11 @@ class Product extends ActiveRecord
         return $this->status == self::STATUS_DRAFT;
     }
 
+    public function getSeoTitle(): string
+    {
+        return $this->meta->title ?: $this->name;
+    }
+
     public function setValue($id, $value): void
     {
         $values = $this->values;